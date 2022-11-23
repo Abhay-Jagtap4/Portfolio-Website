5cc71d84018c19bce3c252bb25c2cd06c53293d7
Abhay Jagtap <abhay.jagtap000@gmail.com>
2022-11-23 19:56:15 +0530

    Completed footer part.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,31 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:9307658711">9307658711</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mainto:abhay.jagtap000@gmail.com">abhay.jagtap000@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+        <SocialIconsContainer>
+          <SocialContainer>
+            <SocialIcons href="https://github.com/Abhay-Jagtap4">
+              <AiFillGithub size="3rem" />
+            </SocialIcons>
+            <SocialIcons href="https://www.linkedin.com/in/abhay-jagtap-765116201/">
+              <AiFillLinkedin size="3rem" />
+            </SocialIcons>
+            <SocialIcons href="https://www.instagram.com/abhayjagtap4/">
+              <AiFillInstagram size="3rem" />
+            </SocialIcons>
+          </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
