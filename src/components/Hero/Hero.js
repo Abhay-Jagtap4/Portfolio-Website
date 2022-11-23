59c4f0b1d8e41356a8ed8ea559e235175e95b122
Abhay Jagtap <abhay.jagtap000@gmail.com>
2022-11-23 17:43:02 +0530

    Completed hero and projects part.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am Abhay Jagtap. I am a 3rd year student studying at IIIT Vadodara.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://github.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
